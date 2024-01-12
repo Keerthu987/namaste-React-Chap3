3efd0b02ac760c8c3bcb30fec45e8af25a73c943
Keerthu987 <81552301+Keerthu987@users.noreply.github.com>
2024-01-12 15:12:16 +0530

    Chap 3 Namste React

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, "Hello");
+const title = (<h1>Title</h1>);
+//Functional Comp
+const TitleComp = ()=> {
+    return(<h1>Title Component</h1>)};
+const HeadComp=()=>{
+return (
+    <div>
+        <h2>{console.log("Hello")}</h2>
+        {title}
+        <TitleComp/>
+        <TitleComp></TitleComp>
+        {TitleComp()}
+
+       <h1>Namaste</h1>
+    </div>
+)
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); 
+root.render(<HeadComp />);
